30a75278d384888484dfb338e1d2d9b8884e002a
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-05-02 21:27:18 -0300

    creación solución y los proyectos

---
+++ Guia5/Ejercicio2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Clear();
+
+            Console.WriteLine("hola mundo!");
+
+            Console.ReadKey();
+        }
+    }
+}
